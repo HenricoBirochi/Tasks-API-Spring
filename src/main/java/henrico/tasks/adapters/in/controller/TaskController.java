8b33e91fb5e9688dd73e71ea238a8131e8004148
Henrico Birochi <henrico.birochi@gmail.com>
2026-01-28 09:11:14 -0300

    feat: Added new route in TaskController to get all Tasks through the taskGroupId

--- src/main/java/henrico/tasks/adapters/in/controller/TaskController.java
+++ src/main/java/henrico/tasks/adapters/in/controller/TaskController.java
@@ -1,20 +1,36 @@
 package henrico.tasks.adapters.in.controller;
 
 import henrico.tasks.application.core.domain.Task;
+import henrico.tasks.application.ports.in.FindAllTasksFromTaskGroupInputPort;
 import henrico.tasks.application.ports.in.InsertTaskInputPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/task")
 public class TaskController {
 
+    public FindAllTasksFromTaskGroupInputPort findAllTasksFromTaskGroupInputPort;
     public InsertTaskInputPort insertTaskInputPort;
 
-    public TaskController(InsertTaskInputPort insertTaskInputPort) {
+    public TaskController(
+            InsertTaskInputPort insertTaskInputPort,
+            FindAllTasksFromTaskGroupInputPort findAllTasksFromTaskGroupInputPort
+    ) {
         this.insertTaskInputPort = insertTaskInputPort;
+        this.findAllTasksFromTaskGroupInputPort = findAllTasksFromTaskGroupInputPort;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Task>> getAllTasks(UUID taskGroupId) {
+        var allTasks = findAllTasksFromTaskGroupInputPort.findAllTasks(taskGroupId);
+        return ResponseEntity.status(200).body(allTasks);
     }
 
     @PostMapping("/")
