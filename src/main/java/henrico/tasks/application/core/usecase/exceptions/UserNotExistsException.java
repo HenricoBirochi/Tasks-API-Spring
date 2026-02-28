222277f401a0e6de9f7f16519f3d073d4b59748d
henricobirochi <henrico.birochi@gmail.com>
2026-02-27 21:31:38 -0300

    feat: Update .gitignore to include userImageUploads and database folders; refactor exception handling by removing UserNotExistsException and adding UserNotFoundException; add configuration for user creation with image upload

--- src/main/java/henrico/tasks/application/core/usecase/exceptions/UserNotExistsException.java
+++
@@ -1,8 +0,0 @@
-package henrico.tasks.application.core.usecase.exceptions;
-
-public class UserNotExistsException extends RuntimeException {
-
-    public UserNotExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/henrico/tasks/config/CreateUserWithImageConfig.java
+++ src/main/java/henrico/tasks/config/CreateUserWithImageConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CreateUserConfig {
+public class CreateUserWithImageConfig {
 
     @Bean
     public CreateUserWithImageUseCase createUserUseCase(
